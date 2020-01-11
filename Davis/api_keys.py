1df3b4dcdf96efe9d11c2167b3c22232ff1c55b0
jauntydl <57422888+jauntydl@users.noreply.github.com>
2020-01-11 03:34:38 -0500

    updated 3.30 am

--- Davis/api_keys.py
+++ Davis/api_keys.py
@@ -2,4 +2,8 @@
 weather_api_key = "534aa190fd2d641455e5358ad7bc6bd5"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBf2HCPIdBHY3BY1xh6qwbbM9lD9FjvFqY"
+
+# aWhere API Key
+aWhere_key = "ZehqCpI6QjcltStruSsPyLp2saIztoA3"
+aWhere_secret = "Jw5sloYwvLLInz6s"
